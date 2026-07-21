a1147abc7eebd7a419777312e1c0ee4d394e9645
YuriTakahashi <s2411258@u.tsukuba.ac.jp>
2026-07-21 19:18:19 +0900

    add group

--- app.js
+++ app.js
@@ -140,24 +140,177 @@ app.get("/list", (req, res) => {
   if (req.session.userId === undefined) {
     return res.redirect("/login");
   }
+
   connection.query(
-    "SELECT comic_name FROM comics JOIN comic_owning ON comics.id = comic_owning.comic_id WHERE comic_owning.user_id = ?",
+    `SELECT comic_name
+     FROM comics
+     JOIN comic_owning
+       ON comics.id = comic_owning.comic_id
+     WHERE comic_owning.group_id = ?`,
+    [req.session.userId],
+    (err, comics) => {
+      if (err) {
+        console.error(err);
+        return res.send(err);
+      }
+
+      connection.query(
+        `SELECT user_groups.group_name, user_groups.id
+         FROM user_groups
+         JOIN group_members
+           ON user_groups.id = group_members.group_id
+         WHERE group_members.user_id = ?`,
+        [req.session.userId],
+        (err, groups) => {
+          if (err) {
+            console.error(err);
+            return res.send(err);
+          }
+
+          res.render("list.ejs", {
+            comics: comics,
+            groups: groups,
+            selectedGroupId: req.query.group_id || "",
+          });
+        },
+      );
+    },
+  );
+});
+
+app.get("/add", (req, res) => {
+  if (req.session.userId === undefined) {
+    return res.redirect("/login");
+  }
+  res.render("add.ejs", { error: null });
+});
+
+app.post("/add", (req, res) => {
+  if (req.session.userId === undefined) {
+    return res.redirect("/login");
+  }
+
+  const comicName = req.body.comic_name;
+  const latest_volume = 1;
+  const author = req.body.author || "";
+  const publisher = req.body.publisher || "";
+
+  const price = req.body.price || 0;
+  const volume = req.body.volume || 1;
+
+  connection.query(
+    "INSERT INTO comics (comic_name, latest_volume, author, publisher) VALUES (?, ?, ?, ?)",
+    [comicName, latest_volume, author, publisher],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.send(err);
+      }
+
+      const comicId = results.insertId;
+
+      connection.query(
+        "INSERT INTO comic_owning (group_id, comic_id, volume, price) VALUES (?, ?, ?, ?)",
+        [req.session.userId, comicId, volume, price],
+        (err) => {
+          if (err) {
+            console.error(err);
+            return res.send(err);
+          }
+
+          res.redirect("/list");
+        },
+      );
+    },
+  );
+});
+
+app.get("/create_group", (req, res) => {
+  if (req.session.userId === undefined) {
+    return res.redirect("/login");
+  }
+  res.render("create_group.ejs", { error: null });
+});
+
+app.post("/create_group", (req, res) => {
+  if (req.session.userId === undefined) {
+    return res.redirect("/login");
+  }
+
+  const groupName = req.body.group_name;
+  const emails = Array.isArray(req.body.emails)
+    ? req.body.emails
+    : req.body.emails
+      ? [req.body.emails]
+      : [];
+  connection.query(
+    "select email from users where id = ?",
     [req.session.userId],
     (err, results) => {
       if (err) {
         console.error(err);
         return res.send(err);
       }
-      res.render("list.ejs", { comics: results });
+      emails.push(results[0].email);
+    },
+  );
+
+  console.log(req.body);
+  console.log(emails);
+
+  connection.query(
+    "INSERT INTO user_groups (group_name, owner_id) VALUES (?, ?)",
+    [groupName, req.session.userId],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.send(err);
+      }
+
+      const groupId = results.insertId;
+
+      emails.forEach((email) => {
+        connection.query(
+          "SELECT id FROM users WHERE email = ?",
+          [email],
+          (err, results) => {
+            if (err) {
+              console.error(err);
+              return res.send(err);
+            }
+
+            if (results.length > 0) {
+              const userId = results[0].id;
+              connection.query(
+                "INSERT INTO group_members (group_id, user_id) VALUES (?, ?)",
+                [groupId, userId],
+                (err) => {
+                  if (err) {
+                    console.error(err);
+                    return res.send(err);
+                  }
+                },
+              );
+            }
+          },
+        );
+      });
+      res.redirect("/home");
     },
   );
 });
 
-app.get("/list/add", (req, res) => {
+app.get("/show_groups", (req, res) => {
   if (req.session.userId === undefined) {
     return res.redirect("/login");
   }
-  res.render("add.ejs", { error: null });
+  connection.query(
+    "SELECT user_groups.group_name from user_groups JOIN group_members ON user_groups.id = group_members.group_id WHERE group_members.user_id = ?",
+    [req.session.userId],
+    (err, results) => {
+      res.render("show_groups.ejs", { error: null, groups: results });
+    },
+  );
 });
 
 app.listen(3000, () => {
