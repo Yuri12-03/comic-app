b9d8305012659185a9d3ce15a2324d91283a685f
YuriTakahashi <s2411258@u.tsukuba.ac.jp>
2026-07-22 16:28:23 +0900

    api access env

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ app.post("/add", async (req, res) => {
       console.log("APP_ID:", process.env.RAKUTEN_APP_ID);
       console.log("ACCESS_KEY:", process.env.RAKUTEN_ACCESS_KEY);
       const response = await axios.get(
-        "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404",
+        "https://openai.rakuten.co.jp/services/api/BooksBook/Search/20170404",
         {
           params: {
             applicationId: process.env.RAKUTEN_APP_ID,
@@ -354,7 +354,7 @@ app.post("/add", async (req, res) => {
       console.log("APP_ID:", process.env.RAKUTEN_APP_ID);
       console.log("ACCESS_KEY:", process.env.RAKUTEN_ACCESS_KEY);
       const response = await axios.get(
-        "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404",
+        "https://openai.rakuten.co.jp/services/api/BooksBook/Search/20170404",
         {
           params: {
             applicationId: process.env.RAKUTEN_APP_ID,
