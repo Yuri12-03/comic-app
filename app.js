7275980afdfcd4b8830cf4b7c83310887c0a8a89
Yuri Takahashi <s2411258@u.tsukuba.ac.jp>
2026-07-22 14:28:31 +0900

    env

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ app.use(
 );
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "YURI",
-  password: "Ageha4/26",
-  database: "comicapp",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 connection.connect((err) => {
